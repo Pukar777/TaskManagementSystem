d33b3ab687a8876d38837ef89b671bd006d80025
ashutoshjak <ashutosh.acharya88@gmail.com>
2023-05-04 14:25:28 +0545

    useContext local storgae save

--- resources/js/components/Auth/AuthContext.jsx
+++ resources/js/components/Auth/AuthContext.jsx
@@ -1,9 +1,29 @@
-import React, { createContext, useState } from "react";
+// import React, { createContext, useState } from "react";
+
+// export const AuthContext = createContext();
+
+// export const AuthProvider = ({children}) => {
+//     const [isLogged, setIsLogged] = useState(false);
+//     return (
+//         <AuthContext.Provider value={{ isLogged, setIsLogged }}>
+//             {children}
+//         </AuthContext.Provider>
+//     );
+// };
+
+import React, { createContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
-    const [isLogged, setIsLogged] = useState(false);
+export const AuthProvider = ({ children }) => {
+    const [isLogged, setIsLogged] = useState(
+        localStorage.getItem("isLogged") === "true"
+    );
+
+    useEffect(() => {
+        localStorage.setItem("isLogged", isLogged);
+    }, [isLogged]);
+
     return (
         <AuthContext.Provider value={{ isLogged, setIsLogged }}>
             {children}
